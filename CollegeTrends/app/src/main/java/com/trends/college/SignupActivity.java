2ae8baa70d793c8fa48283ec56e6846f4dfebfc2
Paramartha <paramarthasaha96@gmail.com>
2018-04-03 17:12:33 +0530

    Refactored android module, Basic navigation designs added

---
+++ CollegeTrends/app/src/main/java/com/trends/college/SignupActivity.java
@@ -0,0 +1,15 @@
+package com.trends.college;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.trends.college.App.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
